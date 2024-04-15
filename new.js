7ba5e33ded18b6380951a816dc9ea93253d0bb89
Arthur Asasira <asasiraarthur@gmail.com>
2024-04-15 17:58:35 +0300

    yg

--- new.js
+++ new.js
@@ -13,32 +13,36 @@
             }
         });
     });
- 
+ //this changes the display attribute of the images divs
     var slide;
     var slides;
     var index=0;
+    //first they are all changed to none then the one with index is changed to block
     function showSlide(index){
         slide.forEach(function(slide){
             slide.style.display='none';
         });
         slide[index].style.display='block';
     }
+    // first checks if there are enough slides then increases index
     function nextSlide(){
         if(index<(slides-1)&&index>=0){
             index+=1;
         };
         showSlide(index);
     }
+    //reduces index
     function prevSlide(){
         if(index<=(slides-1)&&index>0){
             index-=1;
         };
         showSlide(index);
     }
+    //calls function after the page has been loaded not waiting for videos or images
     document.addEventListener('DOMContentLoaded',function(){
         slide=document.querySelectorAll('.slide');
         slides=slide.length;
-        
+        //shows the first slide at index 0
         showSlide(index);
     });
 
